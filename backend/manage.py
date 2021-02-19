526d05a6c3e1f6e242d2b3e26af6cbaec9fb4711
Pegietix <piotr.goldys@estoremedia.com>
2021-02-19 15:01:38 +0100

    Run unit tests before launching server

--- backend/manage.py
+++ backend/manage.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+import pytest
+
 
 def main():
     """Run administrative tasks."""
@@ -15,6 +17,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # Run unit tests before launch.
+    if 'runserver' in sys.argv:
+        os.system('pytest -v')
+
     execute_from_command_line(sys.argv)
 
 
